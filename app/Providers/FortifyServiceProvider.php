3856c71a4e3aa5c25f64f2f27be1ae267576c730
Harris Sontanu <harris.sontanu@gmail.com>
2023-01-19 17:04:01 +0800

    Specifiy User and Member authentication

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -8,9 +8,14 @@ use App\Actions\Fortify\UpdateUserPassword;
 use App\Actions\Fortify\UpdateUserProfileInformation;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Fortify\Fortify;
+use Laravel\Fortify\Contracts\LoginResponse;
+use Laravel\Fortify\Contracts\LogoutResponse;
+use App\Models\User;
+use App\Models\Member;
 
 class FortifyServiceProvider extends ServiceProvider
 {
@@ -19,7 +24,24 @@ class FortifyServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        if (request()->is('admin/*')) {
+            config()->set('fortify.guard', 'admin');
+            config()->set('fortify.home', '/admin/dashboard');
+        }
+
+        $this->app->instance(LoginResponse::class, new class implements LoginResponse {
+            public function toResponse($request)
+            {
+                return redirect()->intended(config('fortify.guard') == 'admin' ? '/admin/dashboard' : '/');
+            }
+        });
+
+        $this->app->instance(LogoutResponse::class, new class implements LogoutResponse {
+            public function toResponse($request)
+            {
+                return redirect(config('fortify.guard') == 'admin' ? '/admin/login' : '/');
+            }
+        });
     }
 
     /**
@@ -31,6 +53,28 @@ class FortifyServiceProvider extends ServiceProvider
         Fortify::updateUserProfileInformationUsing(UpdateUserProfileInformation::class);
         Fortify::updateUserPasswordsUsing(UpdateUserPassword::class);
         Fortify::resetUserPasswordsUsing(ResetUserPassword::class);
+
+        Fortify::authenticateUsing(function (Request $request) {
+            if (config('fortify.guard') === 'admin') {
+
+                $user = User::where('email', $request->email)
+                            ->orWhere('username', $request->email)
+                            ->first();
+
+                if ($user &&
+                    Hash::check($request->password, $user->password)) {
+                    return $user;
+                }
+            } else {
+
+                $member = Member::where('email', $request->email)->first();
+
+                if ($member &&
+                    Hash::check($request->password, $member->password)) {
+                    return $member;
+                }
+            }
+        });
 
         RateLimiter::for('login', function (Request $request) {
             $email = (string) $request->email;
