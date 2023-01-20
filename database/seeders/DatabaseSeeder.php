6fcd24f388fd70f8c994715f9d866211439c1ef6
Harris Sontanu <42680163+harris-sontanu@users.noreply.github.com>
2023-01-20 16:03:57 +0800

    Create Field Model, Migration, Factory and Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,6 +22,7 @@ class DatabaseSeeder extends Seeder
             CategorySeeder::class,
             InstituteSeeder::class,
             MatterSeeder::class,
+            FieldSeeder::class,
         ]);
     }
 }
