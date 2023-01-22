b90d6492667f91a4516c0bd95681235a0364f816
Harris Sontanu <harris.sontanu@gmail.com>
2023-01-22 14:52:44 +0800

    Create Legislation_relationship Model, Migration and Seeder

---
+++ database/migrations/2023_01_22_133611_create_legislation_relationships_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('legislation_relationships', function (Blueprint $table) {
+            $table->bigInteger('legislation_id')->unsigned();
+            $table->bigInteger('related_to')->unsigned();
+            $table->enum('type', ['status', 'legislation', 'document']);
+            $table->string('status')->nullable();
+            $table->text('note')->nullable();
+
+            $table->primary(['legislation_id', 'related_to', 'type']);
+
+            $table->foreign('legislation_id')->references('id')->on('legislations')->cascadeOnDelete();
+            $table->foreign('related_to')->references('id')->on('legislations')->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('legislation_relationships');
+    }
+};
