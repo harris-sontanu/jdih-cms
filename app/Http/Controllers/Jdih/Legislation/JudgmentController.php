15be30d8b56776c8a7bfd9604dce2ea93c65e5ab
Harris Sontanu <harris.sontanu@gmail.com>
2023-02-19 06:34:43 +0800

    Judgment category and index view

--- app/Http/Controllers/Jdih/Legislation/JudgmentController.php
+++ app/Http/Controllers/Jdih/Legislation/JudgmentController.php
@@ -2,10 +2,77 @@
 
 namespace App\Http\Controllers\Jdih\Legislation;
 
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\Jdih\Legislation\LegislationController;
+use App\Http\Traits\VisitorTrait;
 use Illuminate\Http\Request;
+use App\Models\Category;
+use App\Models\Legislation;
 
-class JudgmentController extends Controller
+class JudgmentController extends LegislationController
 {
-    //
+    use VisitorTrait;
+
+    private $categories;
+
+    public function __construct(Request $request)
+    {
+        // Record visitor
+        $this->recordVisitor($request);
+
+        $this->categories = Category::ofType(4)
+            ->sorted()
+            ->pluck('name', 'id');
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $legislations = Legislation::ofType(4)
+            ->with(['category', 'category.type'])
+            ->filter($request)
+            ->published()
+            ->sorted($request)
+            ->paginate($this->limit)
+            ->withQueryString();
+
+        $vendors = [
+            'assets/jdih/js/vendor/forms/selects/select2.min.js',
+        ];
+
+        return view('jdih.legislation.judgment.index', compact(
+            'legislations',
+            'vendors',
+        ))->with('categories', $this->categories)
+            ->with('orderOptions', $this->orderOptions);
+    }
+
+    public function category(Category $category, Request $request)
+    {
+        $legislations = Legislation::ofType(4)
+            ->where('category_id', $category->id)
+            ->published()
+            ->sorted($request)
+            ->latestApproved()
+            ->paginate($this->limit)
+            ->withQueryString();
+
+        $categories = Category::ofType(4)
+            ->sorted()
+            ->pluck('name', 'id');
+
+        $vendors = [
+            'assets/jdih/js/vendor/forms/selects/select2.min.js',
+        ];
+
+        return view('jdih.legislation.judgment.index', compact(
+            'legislations',
+            'category',
+            'vendors',
+        ))->with('categories', $this->categories)
+            ->with('orderOptions', $this->orderOptions);
+    }
 }
---
+++ resources/views/jdih/legislation/judgment/filter.blade.php
@@ -0,0 +1,75 @@
+<!-- Filter -->
+<div class="card shadow-lg">
+    <div class="sidebar-section-header">
+        <h5 class="mb-0">Filter</h5>
+    </div>
+    <div class="sidebar-section-body pt-0">
+        <form class="filter-form" action="{{ route('legislation.monograph.index') }}" method="get">
+            <div class="mb-3">
+                <label for="title" class="d-block form-label fw-semibold">Judul</label>
+                <textarea name="title" id="title" rows="3" cols="20" class="form-control" placeholder="Contoh: covid-19">{{ Request::get('title') }}</textarea>
+            </div>
+
+            <div class="mb-3">
+                <label for="category" class="d-block form-label fw-semibold">Jenis / Bentuk</label>
+                <select id="category" name="category" class="form-select select-search">
+                    <option value="">Pilih Jenis</option>
+                    @foreach ($categories as $key => $value)
+                        <option value="{{ $key }}" @selected(Request::get('category') == $key OR (isset($category) AND $category->id == $key))>
+                            {{ Str::title($value) }}</option>
+                    @endforeach
+                </select>
+            </div>
+
+            <div class="mb-3">
+                <label for="code_number" class="d-block form-label fw-semibold">Nomor Putusan</label>
+                <input type="text" name="code_number" id="code_number" class="form-control"
+                    placeholder="Contoh: 319 PK/Pdt/2016" value="{{ Request::get('code_number') }}">
+            </div>
+
+            <div class="mb-3">
+                <label for="author" class="d-block form-label fw-semibold">T.E.U. Orang / Badan</label>
+                <input type="text" name="author" id="author" class="form-control"
+                    placeholder="Contoh: Satjipto" value="{{ Request::get('author') }}">
+            </div>
+
+            <div class="mb-3">
+                <label for="justice" class="d-block form-label fw-semibold">Jenis Peradilan</label>
+                <input type="text" name="justice" id="justice" class="form-control"
+                    placeholder="Contoh: Pengadilan Agama" value="{{ Request::get('justice') }}">
+            </div>
+
+            <div class="mb-3">
+                <label for="place" class="d-block form-label fw-semibold">Tempat Peradilan</label>
+                <input type="text" name="place" id="place" class="form-control"
+                    placeholder="Contoh: Denpasar" value="{{ Request::get('place') }}">
+            </div>
+
+            <div class="mb-3">
+                <label for="year" class="d-block form-label fw-semibold">Tahun</label>
+                <input type="number" name="year" id="year" class="form-control"
+                    placeholder="Contoh: 2022" value="{{ Request::get('year') }}">
+            </div>
+
+            <div class="mb-3">
+                <label for="subject" class="d-block form-label fw-semibold">Subjek</label>
+                <input type="text" name="subject" id="subject" class="form-control"
+                    placeholder="Contoh: PUNGUTAN LIAR" value="{{ Request::get('subject') }}">
+            </div>
+
+            <div class="mb-3">
+                <label for="field" class="d-block form-label fw-semibold">Bidang Hukum</label>
+                <select id="field" name="field" class="form-select select-search">
+                    <option value="">Pilih Bidang Hukum</option>
+                    @foreach ($fields as $key => $value)
+                        <option value="{{ $key }}" @selected(Request::get('field') == $key)>
+                            {{ Str::title($value) }}</option>
+                    @endforeach
+                </select>
+            </div>
+
+            <button id="filterBtn" type="submit" class="btn btn-danger px-3 fw-semibold w-100">Cari</button>
+        </form>
+    </div>
+</div>
+<!-- /filter -->
