649b2f5b8e8567ebc1ee489a9a1ec7560a2796f3
Harris Sontanu <harris.sontanu@gmail.com>
2023-02-25 06:41:51 +0800

    Popular news

--- resources/views/jdih/post/news/filter.blade.php
+++ resources/views/jdih/post/news/filter.blade.php
@@ -18,9 +18,9 @@
     </div>
 
     <div class="nav nav-sidebar">
-        @foreach ($taxonomies as $taxonomy)            
+        @foreach ($taxonomies as $taxonomy)
             <li class="nav-item">
-                <a href="#" class="nav-link">
+                <a href="{{ route('news.taxonomy', ['taxonomy' => $taxonomy->slug]) }}" class="nav-link">
                     <i class="ph-folder me-2"></i>
                     {{ $taxonomy->name }}
                     <span class="badge bg-teal bg-opacity-10 text-teal rounded-pill ms-auto">{{ $taxonomy->posts->count() }}</span>
@@ -29,4 +29,25 @@
         @endforeach
     </div>
 
+</div>
+<!-- filter -->
+
+<div class="mt-4">
+    <h5 class="fw-bold">Berita Populer</h5>
+
+    <div class="sidebar-section-body px-0 pb-0">
+        @foreach ($popularNews as $news)
+            <div class="d-flex mb-3">
+                <a href="#" class="me-3">
+                    <img src="{{ $news->cover->thumbSource }}" class="rounded shadow" alt="{{ $news->cover->name }}" width="48">
+                </a>
+                <div class="flex-fill">
+                    <h6><a href="{{ route('news.show', ['taxonomy' => $news->taxonomy->slug, 'news' => $news->slug]) }}" class="fw-semibold text-body">{{ $news->title }}</a></h6>
+                    <ul class="list-inline list-inline-bullet text-muted fs-sm">
+                        <li class="list-inline-item"><i class="ph-calendar-blank me-2"></i>{{ $news->dateFormatted($news->published_at) }}</li>
+                    </ul>
+                </div>
+            </div>
+        @endforeach
+    </div>
 </div>
