11803a7c2162fe864c1c4a35f6553f93fc2769db
Harris Sontanu <harris.sontanu@gmail.com>
2023-03-13 06:56:55 +0800

    Implement news show view (done)

--- resources/views/jdih/post/news/show.blade.php
+++ resources/views/jdih/post/news/show.blade.php
@@ -29,12 +29,12 @@
     <div class="content-wrapper">
 
         <!-- Content area -->
-        <main class="content row gx-5">
+        <div class="content row gx-5">
 
             @include('jdih.layouts.like-and-share', ['like' => 5, 'view' => $news->view])
 
-            <div class="col-xl-8">
-                <article class="card shadow-sm mb-4 post-entry">
+            <main class="col-xl-8">
+                <article class="card shadow-sm post-entry">
                     <div class="card-header p-4 border-bottom-0 post-title">
                         <h2 class="d-block display-6 fw-bold mb-2">{{ $news->title }}</h2>
                         <ul class="post-meta list-inline list-inline-bullet text-muted">
@@ -46,13 +46,15 @@
 
                     <figure class="card-image">
                         <img src="{{ $news->cover->source }}" class="figure-img img-fluid shadow" alt="...">
-                        <figcaption class="figure-caption fs-6 px-4">{{ $news->cover->caption }}</figcaption>
+                        @isset($news->cover->caption)
+                            <figcaption class="figure-caption fs-6 px-4">{{ $news->cover->caption }}</figcaption>
+                        @endisset
                     </figure>
 
-                    <div class="card-body fs-5 mb-4 px-4">
+                    <div class="card-body fs-5 px-4">
                         {!! $news->body !!}
 
-                        @if (!empty($news->galleries) AND count($news->galleries) > 0)
+                        @if (isset($news->galleries) AND $news->galleries->count() > 0)
                             <h3 class="fw-bold mt-4">Foto-foto</h3>
                             <div class="row gx-4">
                                 @foreach ($news->galleries as $gallery)
@@ -71,6 +73,10 @@
                                 @endforeach
                             </div>
                         @endif
+
+                        @isset ($news->source)
+                            <p class="mt-4"><span class="fw-bold">Sumber: </span>{{ $news->source }}</p>
+                        @endisset
                     </div>
                 </article>
 
@@ -86,10 +92,112 @@
                         @endforeach
                     </div>
                 @endif
+            </main>
 
-            </div>
+            <aside class="col-xl-3">
 
-        </main>
+                <!-- Popular News -->
+                <div class="mb-4">
+                    <h5 class="fw-bold mb-3">Berita Populer</h5>
+
+                    <div class="sidebar-section-body px-0 pb-0">
+                        @foreach ($popularNews as $news)
+                            <div class="d-flex mb-3 @if (!$loop->last) border-bottom @endif">
+                                <a href="{{ route('news.show', ['taxonomy' => $news->taxonomy->slug, 'post' => $news->slug]) }}" class="me-3">
+                                    <img src="{{ $news->cover->thumbSource }}" class="rounded shadow" alt="{{ $news->cover->name }}" width="48">
+                                </a>
+                                <div class="flex-fill">
+                                    <h6 class="mb-1"><a href="{{ route('news.show', ['taxonomy' => $news->taxonomy->slug, 'post' => $news->slug]) }}" class="fw-semibold text-body">{{ $news->title }}</a></h6>
+                                    <ul class="list-inline list-inline-bullet text-muted fs-sm">
+                                        <li class="list-inline-item"><i class="ph-calendar-blank me-2"></i>{{ $news->dateFormatted($news->published_at) }}</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        @endforeach
+                    </div>
+                </div>
+                <!-- /popular news -->
+
+                <!-- YouTubes -->
+                @if (isset($youtubes) AND $youtubes->count() > 0)
+                    <div class="mb-4">
+                        <h5 class="fw-bold">Video</h5>
+
+                        <div id="carouselExampleIndicators" class="carousel shadow slide" data-bs-ride="carousel">
+                            <div class="carousel-inner">
+                                @foreach ($youtubes as $youtube)
+                                    <div class="carousel-item @if ($loop->first) active @endif">
+                                        <div class="card shadow-0 mb-0">
+                                            <div class="ratio ratio-16x9">
+                                                <iframe src="{{ $youtube->youtubeEmbedSource}}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                                            </div>
+                                            <div class="card-body">
+                                                <h6 class="card-title mb-0">{{ $youtube->title }}</h6>
+                                            </div>
+                                        </div>
+                                    </div>
+                                @endforeach
+                            </div>
+                            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
+                                data-bs-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Previous</span>
+                            </button>
+                            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
+                                data-bs-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Next</span>
+                            </button>
+                        </div>
+                    </div>
+                @endif
+                <!-- /youtubes -->
+
+                <!-- Photos -->
+                @if (isset($photos) AND $photos->count() > 0)
+                    <div class="mb-4">
+                        <h5 class="fw-bold">Galeri Foto</h5>
+                        <div class="row g-0">
+                            @php
+                                $imgClass = null;
+                            @endphp
+                            @foreach ($photos as $photo)
+                                @if ($loop->first)
+                                    <div class="col">
+                                @elseif ($loop->index % 3 === 0)
+                                    </div><div class="col">
+                                @endif
+
+                                @if ($loop->iteration === 1)
+                                    @php $imgClass = 'rounded-top-start'; @endphp
+                                @elseif ($loop->iteration === 3)
+                                    @php $imgClass = 'rounded-bottom-start'; @endphp
+                                @elseif ($loop->iteration === 7)
+                                    @php $imgClass = 'rounded-top-end'; @endphp
+                                @elseif ($loop->iteration === 9)
+                                    @php $imgClass = 'rounded-bottom-end'; @endphp
+                                @else
+                                    @php $imgClass = null; @endphp
+                                @endif
+
+                                <div class="card-img-actions">
+                                    <a href="{{ $photo->source }}" class="text-white" data-bs-popup="lightbox">
+                                        <img class="img-fluid {{ $imgClass }}" src="{{ $photo->thumbSource }}" alt="{{ $photo->title }}">
+                                        <span class="card-img-actions-overlay {{ $imgClass }}">
+                                            <i class="ph-plus"></i>
+                                        </span>
+                                    </a>
+                                </div>
+                            @endforeach
+
+                        </div>
+                    </div>
+                @endif
+                <!-- /photos -->
+
+            </aside>
+
+        </div>
         <!-- /content area -->
 
     </div>
